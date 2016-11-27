f73752b1424edc337b4eee2a9ec77bac63edae6e
kejace <kejace@gmail.com>
2016-11-26 20:31:28 -0500

    Uses SecInt. Added GHCJS capabilities

--- app/paillier.hs
+++ app/paillier.hs
@@ -1,12 +1,39 @@
-import System.Random
+{-# LANGUAGE PartialTypeSignatures #-}
+
+import Control.Monad.Random.Class
 import Primes
 
-data PublicKey = Pub Integer Integer
+import Control.Monad.State (State, evalState, get, put)
+import System.Random -- (StdGen, mkStdGen, random)
+import Control.Applicative ((<$>))
+
+data PublicKey a = Pub Integer Integer
     deriving Show
-data PrivateKey = Priv Integer Integer
+data PrivateKey a = Priv Integer Integer
     deriving Show
 
-keys :: RandomGen g => g -> Int -> (PublicKey, PrivateKey, g)
+data SecInt = Sec Integer
+
+instance Monoid SecInt where
+    mempty  = undefined
+    mappend = undefined
+
+instance Show SecInt where
+   show (Sec i) = "Sec" ++ show i
+
+instance Functor PublicKey where
+    fmap _ (Pub i j) = Pub i j
+    --fmap f (Pub i j) = Pub (f i j)
+
+--instance Foldable PublicKey where
+
+type R a = State StdGen a
+
+runRandom :: R a -> Int -> a
+runRandom action seed = evalState action $ mkStdGen seed
+
+keys :: RandomGen g => 
+        g -> Int -> (PublicKey a, PrivateKey b, g)
 keys g sizeBits = (Pub n g2, Priv lambda mu, g')
     where
         kLen = fromIntegral $ sizeBits `div` 8
@@ -16,10 +43,12 @@ keys g sizeBits = (Pub n g2, Priv lambda mu, g')
         mu = modInverse lambda n
         g2 = n + 1
 
+modInverse :: Integer -> Integer -> Integer
 modInverse 1 _ = 1
 modInverse x y = (n * y + 1) `div` x
     where n = x - modInverse (y `mod` x) x
 
+modPow :: Integer -> Integer -> Integer -> Integer
 modPow m = pow' (modMul m) (modSquare m)
     where
         modMul m a b = (a * b) `mod` m
@@ -35,24 +64,34 @@ modPow m = pow' (modMul m) (modSquare m)
                         (q, r) = quotRem n 2
                         x2 = s x
 
+encrypt :: RandomGen t => 
+           t -> Integer -> PublicKey t1 -> (SecInt, t)
 encrypt rg m (Pub n g) = (c, rg')
     where
         (r, rg') = large_random_prime rg 32
         n2 = n ^ 2
         x = (modPow n2 r n)
-        c = ((modPow n2 g m) * x) `mod` n2
+        c = Sec $ ((modPow n2 g m) * x) `mod` n2
 
-decrypt c (Pub n g) (Priv lambda mu) = p
+decrypt :: SecInt -> PublicKey t -> PrivateKey t1 -> Integer
+decrypt (Sec c) (Pub n g) (Priv lambda mu) = p 
     where
         n2 = n ^ 2
         x = modPow n2 c lambda - 1
         p = ((x `div` n) * mu) `mod` n
 
-pAdd a b (Pub n g) = (a * b) `mod` (n ^ 2)
-pAddPlain a b (Pub n g) = a * (modPow (n ^ 2) g b)
-pMulPlain a b (Pub n g) = modPow (n ^ 2) a b
+pAdd :: SecInt -> SecInt -> PublicKey t -> SecInt
+pAdd (Sec a) (Sec b) (Pub n g) = Sec $ (a * b) `mod` (n ^ 2) 
 
+pAddPlain :: SecInt -> Integer -> PublicKey t -> SecInt
+pAddPlain (Sec a) b (Pub n g) = Sec $ a * (modPow (n ^ 2) g b)
+
+pMulPlain :: SecInt -> Integer -> PublicKey t -> SecInt
+pMulPlain (Sec a) b (Pub n g) = Sec $ modPow (n ^ 2) a b
+
+main :: IO ()
 main = do
+
     g <- getStdGen
 
     let (pub, priv, g') = keys g 256
@@ -65,15 +104,16 @@ main = do
         r = pAdd c1 c2 pub
         r' = decrypt r pub priv
 
-        s = pAddPlain c1 18 pub
-        s' = decrypt s pub priv
+        -- s = pAddPlain c1 18 pub
+        -- s' = decrypt s pub priv
 
-        t = pMulPlain c1 3 pub
-        t' = decrypt t pub priv
+        -- t = pMulPlain c1 3 pub
+        -- t' = decrypt t pub priv
 
-    putStrLn ((show m1) ++ " becomes " ++ (show c1))
-    putStrLn ((show m2) ++ " becomes " ++ (show c2))
+    putStrLn $ (show m1) ++ " becomes " ++ (show c1)
+    putStrLn $ (show m2) ++ " becomes " ++ (show c2)
+    putStrLn $ "Their sum is: " ++ (show r)
 
-    putStrLn ((show m1) ++ " + " ++ (show m2) ++ " = " ++ (show r'))
-    putStrLn ((show m1) ++ " + 18 = " ++ (show s'))
-    putStrLn ((show m1) ++ " * 3 = " ++ (show t'))
+    putStrLn $ (show m1) ++ " + " ++ (show m2) ++ " = " ++ (show r')
+    -- putStrLn ((show m1) ++ " + 18 = " ++ (show s'))
+    -- putStrLn ((show m1) ++ " * 3 = " ++ (show t'))
