5c40e061f7c997ae23e79d6e70ea960b24b3803d
kejace <kejace@gmail.com>
2017-01-28 21:07:50 -0500

    Linted

--- app/paillier.hs
+++ app/paillier.hs
@@ -37,7 +37,7 @@ data PublicKey = Pub {n :: Integer,  g :: Integer}
 data PrivateKey = Priv {lambda :: Integer, mu :: Integer}
     deriving Show
 
-type PublicEnv m = (MonadReader (PublicKey) m, MonadState (StdGen) m)
+type PublicEnv m = (MonadReader PublicKey m, MonadState StdGen m)
 
 data SecInt = Sec Integer
 data SecInt' = Sec' Integer PublicKey
@@ -46,7 +46,7 @@ data SecInt' = Sec' Integer PublicKey
 ---  (<>) :: m a -> m a -> m a
 
 instance Show SecInt where
-   show (Sec i) = (BC.unpack $ B16.encode $ B.pack $ integer2Bytes $ fromIntegral i)
+   show (Sec i) = BC.unpack $ B16.encode $ B.pack $ integer2Bytes $ fromIntegral i
 
 -- decryptShow :: (MonadReader PublicKey m) => 
 --                SecInt -> PrivateKey -> m String
@@ -64,7 +64,7 @@ instance (MonadReader PublicKey m) => Semigroup (m SecInt) where
             return $ Sec $ (x * y)  `mod` (z ^ 2)
 
 type RandEnv a = State StdGen a
-type PubKeyEnv = Reader (PublicKey)
+type PubKeyEnv = Reader PublicKey
 
 runRandom :: RandEnv a -> Int -> a
 runRandom action seed = evalState action $ mkStdGen seed
@@ -80,35 +80,32 @@ keys g sizeBits = (Pub n g2, Priv lambda mu, g')
     mu = modInverse lambda n
     g2 = n + 1
 
-keysM :: (MonadState (StdGen) m) =>
+keysM :: (MonadState StdGen m) =>
           Int -> m (PublicKey, PrivateKey)
-keysM sizeBits = do
-    g <- get
-    let (pub, priv, g') = keys g sizeBits
-    put g'
-    return $ (pub, priv)
+keysM sizeBits = do g <- get
+                    let (pub, priv, g') = keys g sizeBits
+                    put g'
+                    return (pub, priv)
 
 encrypt :: RandomGen t =>
-           t -> Integer -> (PubKeyEnv) (SecInt, t)
-encrypt rg m = do
-    (Pub n g) <- ask
-    let (r, rg') = large_random_prime rg 32
-    let n2 = n ^ 2
-    let x = (modPow n2 r n)
-    let c = Sec $ ((modPow n2 g m) * x) `mod` n2
-    return (c, rg')
+           t -> Integer -> PubKeyEnv (SecInt, t)
+encrypt rg m = do (Pub n g) <- ask
+                  let (r, rg') = large_random_prime rg 32
+                  let n2 = n ^ 2
+                  let x = (modPow n2 r n)
+                  let c = Sec $ (modPow n2 g m * x) `mod` n2
+                  return (c, rg')
 
 encryptM :: (PublicEnv m) =>
              Integer -> m SecInt
-encryptM i = do
-    (Pub n g) <- ask
-    rg <- get
-    let (r, rg') = large_random_prime rg 32
-    let n2 = n ^ 2
-    let x = (modPow n2 r n)
-    let c = Sec $ ((modPow n2 g i) * x) `mod` n2
-    put rg'
-    return $ c
+encryptM i = do (Pub n g) <- ask
+                rg <- get
+                let (r, rg') = large_random_prime rg 32
+                let n2 = n ^ 2
+                let x = (modPow n2 r n)
+                let c = Sec $ (modPow n2 g i * x) `mod` n2
+                put rg'
+                return c
 
 decryptM :: (MonadReader PublicKey m) =>
             SecInt -> PrivateKey -> m Integer
@@ -118,16 +115,16 @@ decryptM (Sec c) (Priv lambda mu) = do
     let p = ((x `div` n) * mu) `mod` n
     return p
 
-pAdd :: SecInt -> SecInt -> (PubKeyEnv) SecInt
+pAdd :: SecInt -> SecInt -> PubKeyEnv SecInt
 pAdd (Sec a) (Sec b) = do
     n <- asks n
-    return $ Sec $ (a * b) `mod` (n ^ 2) 
+    return $ Sec $ (a * b) `mod` (n ^ 2)
 
 pAddM :: (MonadReader PublicKey m) =>
          SecInt -> SecInt -> m SecInt
 pAddM (Sec a) (Sec b) = do
     n <- asks n
-    return $ Sec $ (a * b) `mod` (n ^ 2)     
+    return $ Sec $ (a * b) `mod` (n ^ 2)
 
 -- if we can derive foldl1 from Semigroup
 -- but we could also just treat 0 as (Sec 0) and
@@ -143,16 +140,16 @@ pAddMl :: (MonadReader PublicKey m) =>
            m SecInt -> SecInt -> m SecInt
 pAddMl a (Sec b) = do
     nn <- asks n
-    aa <- ((*) b) <$> (f <$> a)
-    return $ Sec $ aa `mod` (nn ^ 2) 
+    aa <- (*) b <$> (f <$> a)
+    return $ Sec $ aa `mod` (nn ^ 2)
       where
-    f :: SecInt -> Integer 
-    f (Sec a) = a 
+    f :: SecInt -> Integer
+    f (Sec a) = a
 
 ----------------------------------
 
 pAddPlain :: SecInt -> Integer -> PublicKey -> SecInt
-pAddPlain (Sec a) b (Pub n g) = Sec $ a * (modPow (n ^ 2) g b)
+pAddPlain (Sec a) b (Pub n g) = Sec $ a * modPow (n ^ 2) g b
 
 pMulPlain :: SecInt -> Integer -> PublicKey -> SecInt
 pMulPlain (Sec a) b (Pub n g) = Sec $ modPow (n ^ 2) a b
@@ -169,12 +166,10 @@ mainEnv = do
     g <- getStdGen
 
     let (pub, priv, g') = keys g 256
- 
     let q = flip runReader pub $ do
                 (c1, g'') <- encrypt g' m1
                 (c2, _) <- encrypt g'' m2
-                toRet <- pAdd c1 c2 
-                return $ toRet
+                pAdd c1 c2
 
         (c1, g'') = runReader (encrypt g' m1) pub
         (c2, _) = runReader (encrypt g'' m2) pub
@@ -182,9 +177,9 @@ mainEnv = do
         r = runReader (pAdd c1 c2) pub :: SecInt
         r'' = runReader (decryptM r priv) pub :: Integer
 
-    putStrLn $ (show m1) ++ " becomes " ++ (show c1)
-    putStrLn $ (show m2) ++ " becomes " ++ (show c2)
-    putStrLn $ "Their sum is: " ++ (show r) ++ " = " ++ (show r'')
+    putStrLn $ show m1 ++ " becomes " ++ show c1
+    putStrLn $ show m2 ++ " becomes " ++ show c2
+    putStrLn $ "Their sum is: " ++ show r ++ " = " ++ show r''
 
 mainMonad :: IO ()
 mainMonad = do
@@ -193,43 +188,41 @@ mainMonad = do
 
     let ((pub, priv), g') = runState (keysM 256) g
 
-    _ <- flip runReaderT pub $ do 
+    _ <- flip runReaderT pub $ do
 
           (c1, c2) <- flip evalStateT g' $ do
             c1 <- encryptM m1
             c2 <- encryptM m2
             return (c1, c2)
 
-          r <- pAddM c1 c2 
+          r <- pAddM c1 c2
 
-          liftIO $ putStrLn $ (show m1) ++ " becomes " ++ (show c1)
-          liftIO $ putStrLn $ (show m2) ++ " becomes " ++ (show c2)
-          
+          liftIO $ putStrLn $ show m1 ++ " becomes " ++ show c1
+          liftIO $ putStrLn $ show m2 ++ " becomes " ++ show c2
+
           r'' <- decryptM r priv
-          
-          liftIO $ putStrLn $ "\nTheir sum is: " ++ (show r) ++ " = " ++ (show r'')
-         
-          let ms' = Prelude.take 999 $ [1..]
+
+          liftIO $ putStrLn $ "\nTheir sum is: " ++ show r ++ " = " ++ show r''
+
+          let ms' = Prelude.take 999 [1..]
 
           (l, l0) <- flip evalStateT g' $ do
-            l <- sequence $ encryptM <$> ms' 
+            l <- sequence $ encryptM <$> ms'
             l0 <- encryptM 0
-            return $ (l, l0)
+            return (l, l0)
 
-          es <- foldl pAddMl (pure l0) l 
+          es <- foldl pAddMl (pure l0) l
           des <- decryptM es priv
-          liftIO $ putStrLn $ "\ntake " ++ (show $ Prelude.length ms') ++ " [1..]" 
-          liftIO $ putStrLn $ "\nThe (foldl) sum is: " ++ (show es) ++ " = " ++ (show des)
-         
-          ll <- flip evalStateT g' $ do
-            toRet <- sequence $ encryptM <$> fromList ms'
-            return toRet
-       
-          es' <- sconcat (pure <$> ll) 
+          liftIO $ putStrLn $ "\ntake " ++ show (Prelude.length ms') ++ " [1..]"
+          liftIO $ putStrLn $ "\nThe (foldl) sum is: " ++ show es ++ " = " ++ show des
+
+          ll <- flip evalStateT g' $ sequence $ encryptM <$> fromList ms'
+
+          es' <- sconcat (pure <$> ll)
           des' <- decryptM es' priv
-          liftIO $ putStrLn $ "\nThe (sconcat) sum is: " ++ (show es') ++ " = " ++ (show des')
-          
-          return () 
+          liftIO $ putStrLn $ "\nThe (sconcat) sum is: " ++ show es' ++ " = " ++ show des'
+
+          return ()
 
     return ()
 
